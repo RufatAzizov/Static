e36530daa44ab18895c35ce15033eb01fe4eb936
RufatAzizov <mi6wkrck6@code.edu.az>
2023-11-03 01:55:28 +0400

    First commit

---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User(1, "Rufat", 30);
+
+            Type userType = user.GetType();
+            FieldInfo idField = userType.GetField("id", BindingFlags.NonPublic | BindingFlags.Instance);
+            FieldInfo nameField = userType.GetField("name", BindingFlags.NonPublic | BindingFlags.Instance);
+            FieldInfo ageField = userType.GetField("age", BindingFlags.NonPublic | BindingFlags.Static);
+
+            idField.SetValue(user, 2);
+            nameField.SetValue(user, "Rufat");
+            ageField.SetValue(user, 35);
+
+            Console.WriteLine("ID: " + user.GetType().GetField("id", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(user));
+            Console.WriteLine("Name: " + user.GetType().GetField("name", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(user));
+            Console.WriteLine("Age: " + User.ChangeAge);
+        }
+    }
+}
---
+++ Reflection/Reflection/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class User
+    {
+        private int id;
+        private string name;
+        private static int age;
+
+        public User(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            User.age = age;
+        }
+
+        public static void ChangeAge(int newAge)
+        {
+            age = newAge;
+        }
+    }
+}
